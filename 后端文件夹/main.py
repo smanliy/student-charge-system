1c712676c261f79cfae5f5670472d2655b8b0ff5
Halit <Halitum0216@gmail.com>
2024-02-21 13:25:54 +0800

    小补丁

--- 后端文件夹/main.py
+++ 后端文件夹/main.py
@@ -3,12 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from students_routes import router as students_router
 from awards_routes import router as awards_router
 from position_routes import router as position_router
+from login_register_routes import router as login_register_router
 
 app = FastAPI()
 
 app.include_router(students_router)
 app.include_router(awards_router)
 app.include_router(position_router)
+app.include_router(login_register_router)
 
 # 配置CORS
 app.add_middleware(
