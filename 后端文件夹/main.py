aa1b37d13580d2368eebd00c41b73b86c4d7dd23
Halit <Halitum0216@gmail.com>
2024-02-21 11:21:36 +0800

    后端-赵云-ver0.1-职位信息管理模块

--- 后端文件夹/main.py
+++ 后端文件夹/main.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from students_routes import router as students_router
 from awards_routes import router as awards_router
+from position_routes import router as position_router
 
 app = FastAPI()
 
 app.include_router(students_router)
 app.include_router(awards_router)
+app.include_router(position_router)
 
 # 配置CORS
 app.add_middleware(
