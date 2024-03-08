be27447660fa14e60742d63d1b46c9aed170a334
zyx <zyx555zyx@qq.com>
2024-03-09 00:52:13 +0800

    zyx-token_text3

--- 前端文件夹/scripts/登录注册.js
+++ 前端文件夹/scripts/登录注册.js
@@ -10,8 +10,8 @@ function check() {
   // 请求拦截器  
   service.interceptors.request.use(  
     config => {  
-      const token = localStorage.getItem('token');
-     console.log(token) 
+      // const token = localStorage.getItem('token');
+     console.log("111") 
      if (localStorage.getItem('token')) {
       config.headers.Authorization = localStorage.getItem('token')
   }
