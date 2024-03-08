29df95af98c7764eb6ff5a3ae55835a1d4a4c0b1
zyx <zyx555zyx@qq.com>
2024-03-08 19:13:14 +0800

    zyx-解决学生端账户问题

--- 前端文件夹/scripts/增加信息.js
+++ 前端文件夹/scripts/增加信息.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   var searchSystemTwo = document.getElementById("search-system-2");  
   if (searchSystemTwo) {  
     searchSystemTwo.onclick = function() {  
-      window.location.href="http://101.200.73.250/direction_number.html"
+      window.location.href="http://101.200.73.250/charge-students-menu.html"
     };  
   } else {  
     console.error("元素未找到");  
